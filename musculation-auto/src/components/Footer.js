a49df6b7a045cbf723849ba292b305e05372d384
Ozheim <heikel.zouaoui@gmail.com>
2024-07-22 00:42:16 +0200

    Work in Progress: Implement Accordion in Footer

--- musculation-auto/src/components/Footer.js
+++ musculation-auto/src/components/Footer.js
@@ -1,27 +1,37 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import "../Styles/components/Footer.scss";
 
-class Footer extends Component {
-  render() {
-    return (
-      <div>
-        <section>
-          <div className="Newsletter">
-            <form action="?" method="POST">
-              <label for="email">Newsletter</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="your-email@site.com"
-                required
-              />
-              <button type="submit">Envoyer</button>
-            </form>
-          </div>
-        </section>
+const Footer = () => {
+  const [isActive, setIsActive] = useState(false);
+  const AccordionFooter = (
+    <div className="footer-container">
+      <>
+        <div className="Newsletter">
+          <form action="?" method="POST">
+            <label for="email">Newsletter</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="your-email@site.com"
+              required
+            />
+            <button type="submit">Envoyer</button>
+          </form>
+        </div>
+      </>
+    </div>
+  );
+
+  return (
+    <>
+      <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
+        <div>YOOO </div>
+        <div>+</div>
       </div>
-    );
-  }
-}
+      {isActive && <div className="accordion">{AccordionFooter}</div>}
+    </>
+  );
+};
 
 export default Footer;
