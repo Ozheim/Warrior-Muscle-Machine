a49c234c4202e0e684c8cfe33c7ff46fe7f33dea
Ozheim <heikel.zouaoui@gmail.com>
2024-07-25 23:36:00 +0200

    Footer finished

--- musculation-auto/src/components/Footer.js
+++ musculation-auto/src/components/Footer.js
@@ -1,31 +1,45 @@
 import React from "react";
 import "../Styles/components/Footer.scss";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebookF,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 
 export function Footer() {
   return (
     <div className="footer-container">
       <>
-        <div className="Newsletter">
+        <div className="newsletter">
           <form action="?" method="POST">
             <label for="email">Newsletter</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="your-email@site.com"
-              required
-            />
-            <button type="submit">Envoyer</button>
+            <div className="input-group">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="your-email@site.com"
+                required
+              />
+              <button type="submit">
+                <FontAwesomeIcon
+                  icon={faPaperPlane}
+                  style={{ color: "#FFD43B" }}
+                />
+              </button>
+            </div>
           </form>
         </div>
         <>
-          <ul>
+          <ul className="link-list">
             <li>
               <Link to="/Home">Accueil</Link>
             </li>
             <li>
-              <Link to="/Nouveauté">Prochainement</Link>
+              <Link to="/Nouveauté">Nouveauté</Link>
             </li>
             <li>
               <Link to="/About">A propos</Link>
@@ -38,6 +52,17 @@ export function Footer() {
             </li>
           </ul>
         </>
+        <ul className="social-media-list">
+          <li>
+            <FontAwesomeIcon icon={faFacebookF} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faTwitter} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faYoutube} />
+          </li>
+        </ul>
       </>
     </div>
   );
