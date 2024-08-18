d7dc6616c6df3b6646b2d4c588cf9f3d2a319b58
Ozheim <heikel.zouaoui@gmail.com>
2024-08-18 22:13:09 +0200

    Fixed 404 error on /home route, added user schema

--- musculation-auto/Backend/models/User.js
+++ musculation-auto/Backend/models/User.js
@@ -1,1 +1,6 @@
 const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
