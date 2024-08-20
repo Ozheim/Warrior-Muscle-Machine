496af9728159b35edd8e3763628e2877105d83a1
Ozheim <heikel.zouaoui@gmail.com>
2024-08-20 20:48:45 +0200

    Fix 500 Internal Server Error: Resolved issue with user creation logic

--- musculation-auto/Backend/App.js
+++ musculation-auto/Backend/App.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGODB, {
--- musculation-auto/Backend/controllers/User.js
+++ musculation-auto/Backend/controllers/User.js
@@ -1,3 +1,4 @@
+const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
 exports.signup = (req, res, next) => {
